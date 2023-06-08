4d320d319de41eebae6fbed88a79c3af31e158df
endingofourlife <endingofourlife@gmail.com>
2023-06-08 21:12:21 +0300

    Implement AddIdentityServices method for configuring identity services

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        /// <summary>
+        /// Configures identity services for the application, including setting up identity core with custom options and adding Entity Framework stores.
+        /// </summary>
+        /// <returns>The modified <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<User>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<DataContext>();
+
+            return services;
+        }
+    }
+}
