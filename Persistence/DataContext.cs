8fee89f71b71dbd3c1dbb65823c8ab46cccefc83
endingofourlife <endingofourlife@gmail.com>
2023-06-08 21:13:51 +0300

    Added method to populate initial user data in the database

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Url> Urls { get; set; }
+    }
+}
---
+++ Persistence/Seed.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context, UserManager<User> userManager)
+        {
+            if(!userManager.Users.Any())
+            {
+                var users = new List<User>
+                {
+                    new User {UserName="Bob", Email="bob@test.com", IsAdmin=true},
+                    new User {UserName="Alice", Email="alice@test.com", IsAdmin=false}
+                };
+                foreach(var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+            await context.SaveChangesAsync();
+        }
+    }
+}
