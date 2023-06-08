11180962498900a026bac8d40799420458cfb750
endingofourlife <endingofourlife@gmail.com>
2023-06-08 21:18:56 +0300

    updated: Urls property, added initial value

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class User
+    public class User : IdentityUser
     {
         public bool IsAdmin { get; set; }
-        public ICollection<Url> Urls { get; set; }
+        public ICollection<Url> Urls { get; set; } = new List<Url>();
     }
 }
