6ce192ca7d28011016b0cf85c73f862ca8756fb2
endingofourlife <endingofourlife@gmail.com>
2023-06-09 15:01:12 +0300

    Simplified Program class by separating services into extension classes

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Application.Urls;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            
+            //MySql database
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            //Cors policy
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler));
+            return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using Application.Urls;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
@@ -8,17 +9,9 @@ using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+
 
 var app = builder.Build();
 
@@ -28,6 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
