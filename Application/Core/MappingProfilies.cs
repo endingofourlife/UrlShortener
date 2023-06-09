b6584c4e83839c8bd9836194b8782ec4f4c39dc1
endingofourlife <endingofourlife@gmail.com>
2023-06-09 15:06:05 +0300

    added automapping basics

---
+++ Application/Core/MappingProfilies.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain;
+
+namespace Application.Core
+{
+    public class MappingProfilies : Profile
+    {
+        public MappingProfilies()
+        {
+            // TODO 
+        }
+    }
+}
