63a932c7c441795ebd9170d619a9e5ce8a471797
endingofourlife <endingofourlife@gmail.com>
2023-06-09 15:01:45 +0300

    Added url DTO model

---
+++ API/DTOs/UrlDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace API.DTOs
+{
+    public class UrlDto
+    {
+        public string OriginalUrl { get; set; }
+        public string ShortUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UserId { get; set; }
+    }
+}
