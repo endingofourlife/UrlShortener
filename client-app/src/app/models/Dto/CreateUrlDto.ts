2fff823f41c460df2be5c334f5cbcf6e0f4a02bb
endingofourlife <endingofourlife@gmail.com>
2023-06-12 14:39:33 +0300

    Added a few models -> UsersUrl and CreateUrlDto

---
+++ client-app/src/app/models/Dto/CreateUrlDto.ts
@@ -0,0 +1,5 @@
+export interface CreateUrlDto {
+    originalUrl: string;
+    shortUrl: string;
+    userId: string;
+}
---
+++ client-app/src/app/models/UsersUrl.ts
@@ -0,0 +1,7 @@
+export interface UsersUrl {
+    id: string;
+    originalUrl: string;
+    shortUrl: string;
+    createdDate: string;
+    userId: string;
+}
