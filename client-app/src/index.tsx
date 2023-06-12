455545841b82298dc75577167729e4e1d802d0ce
endingofourlife <endingofourlife@gmail.com>
2023-06-12 14:39:00 +0300

    updated design of layout on client

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'semantic-ui-css/semantic.min.css'
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from "./reportWebVitals";
+import {store, StoreContext} from './app/stores/store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+ReactDOM.render(
+    <StoreContext.Provider value={store}>
+        <App />
+    </StoreContext.Provider>,
+    document.getElementById('root')
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
 
 reportWebVitals();
