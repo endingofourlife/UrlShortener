abcea3195b24823e6ebaa4ba9bee5e15d2f68bcb
endingofourlife <endingofourlife@gmail.com>
2023-06-12 21:07:47 +0300

    Updated about page.

--- client-app/src/features/about/AboutPage.tsx
+++ client-app/src/features/about/AboutPage.tsx
@@ -4,6 +4,13 @@ export default function AboutPage() {
     return (
         <Container style={{marginTop: '7em'}}>
             <h1>About page</h1>
+            <p>The URL Shortener project aims to create an application that can shorten any URL and provide a shortened equivalent for easy navigation.
+                The project includes several views, such as the Login view,
+                Short URLs Table view, My shortcuts view, and About view.
+            </p>
+            <p style={{color:"red"}}>
+                Please note that the login functionality and user roles/permissions, my shortcuts have not been implemented yet.
+            </p>
         </Container>
     )
 }
--- client-app/src/features/urls/dashboard/UrlList.tsx
+++ client-app/src/features/urls/dashboard/UrlList.tsx
@@ -16,28 +16,43 @@ export default observer(function UrlList() {
         deleteUrl(id);
     }
 
+    function getFormattedUrl(url: UsersUrl) {
+        if (url.shortUrl.startsWith("http://localhost")) {
+            return url.shortUrl;
+        } else {
+            return `http://localhost:5000/api/Shortcut/${url.shortUrl}`;
+        }
+    }
     return (
         <>
-            <Item.Group divided>
-                {urlsByDate.map((url, index) => (
-                    <Item key={url.id}>
-                        <Item.Content>
-                            <Item.Header>{index + 1}. {url.originalUrl}</Item.Header>
-                            <Item.Description>
-                                <div className="url-details">
-                                    <div className="url-date">{url.createdDate}</div>
-                                    <div className="url-arrow">➡</div>
-                                    <div className="url-short">{url.shortUrl}</div>
-                                </div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button floated='right' content='View' color='blue' onClick={()=>openUrlDetails(url)} />
-                                <Button name={url.id} loading={loading && target===url.id} floated='right' content='Delete' color='red' onClick={(e)=>handleUrlDelete(e, url.id)} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
+            {urlsByDate.length === 0 ? (
+                <div style={{fontSize:'24px', color: "blue", height:'300px', display:'flex', justifyContent:'center', alignItems:'center'}}>There are no URLs. Create the first one.</div>
+            ) : (
+                <Item.Group divided>
+                    {urlsByDate.map((url, index) => (
+                        <Item key={url.id}>
+                            <Item.Content>
+                                <Item.Header>{index + 1}. {url.originalUrl}</Item.Header>
+                                <Item.Description>
+                                    <div className="url-details">
+                                        <div className="url-date">{url.createdDate}</div>
+                                        <div className="url-arrow">➡</div>
+                                        <div className="url-short">
+                                            <a href={getFormattedUrl(url)} target="_blank" rel="noopener noreferrer">
+                                                {url.shortUrl}
+                                            </a>
+                                        </div>
+                                    </div>
+                                </Item.Description>
+                                <Item.Extra>
+                                    <Button floated='right' content='View' color='blue' onClick={() => openUrlDetails(url)} />
+                                    <Button name={url.id} loading={loading && target === url.id} floated='right' content='Delete' color='red' onClick={(e) => handleUrlDelete(e, url.id)} />
+                                </Item.Extra>
+                            </Item.Content>
+                        </Item>
+                    ))}
+                </Item.Group>
+            )}
         </>
     )
 })
